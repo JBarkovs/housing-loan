60b74d57f81acf240341b89f520d575e0356ae91
jbarkovs <janisbarkovs@gmail.com>
2023-05-23 23:31:26 +0300

    Add form to client and pass loan data to server call

--- loan-client/src/app/app.component.ts
+++ loan-client/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core'
+import {Component} from '@angular/core'
+import {FormBuilder, Validators} from "@angular/forms"
+import {HttpClient} from "@angular/common/http"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'loan-client'
+export class AppComponent{
+  //TODO add global FormBuilderService.getForm() passing controlsConfig
+  form = this.fb.group({
+    principleAmount: ['', Validators.required],
+    periodYears: ['', Validators.required],
+    loanTypeId: ['']
+  })
+
+  constructor(private httpClient: HttpClient, private fb: FormBuilder) {
+  }
+
+  onSubmit(): void {
+    //TODO get loanTypeId from parent component or from a server call
+    this.form.controls['loanTypeId'].setValue(1)
+
+    //TODO add global httpService with .get() .post() ...
+    this.httpClient.get('http://localhost:8080/rest/housing', {
+      params: this.form.value
+    })
+      .subscribe(response => {
+        console.log(response)})
+  }
 }
